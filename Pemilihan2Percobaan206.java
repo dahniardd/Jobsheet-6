47a0935453d671d274790123b8dd79a4cf0b2bec
dahniardd <dahniardvn09@gmail.com>
2023-10-05 14:11:06 +0700

    Modifikasi Pemilihan 2 Percobaan 2

--- Pemilihan2Percobaan206.java
+++ Pemilihan2Percobaan206.java
@@ -17,19 +17,13 @@ public class Pemilihan2Percobaan206 {
         if(totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
                System.out.println("Segitiga tersebut adalah segitiga siku - siku");
-            else
-               System.out.println("Segitiga tersebut bukan segitiga siku - siku");
-        }else if (totalSudut == 180){
-            if  ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60))
-               System.out.println("Segitiga tersebut adalah segitiga sama sisi");
-            else
-               System.out.println("Segitiga tersebut bukan segitiga sama sisi");
-        }else if (totalSudut == 180){
-            if ((sudut1 == 40) && (sudut2 == 70) && (sudut3 == 70))
+            else if (sudut1 == sudut2 && sudut2 == sudut3)
+               System.out.println("Segitiga tersesebut adalah segitiga sama sisi");
+            else if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3)
                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
             else
-               System.out.println("Segitiga tersebut bukan segitiga sama kaki");
+               System.out.println("Segitiga tersebut adalah segitiga sembarang");
         }else
-            System.out.println("Bukan Segitiga");
+        System.out.println("Bukan Segitiga");
     }
 }
